279485f4eefe4bd53d3ec1007ab271b1996e5135
James He <he.is.james7@gmail.com>
2025-04-11 23:50:25 -0700

    General structure styling for ordering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { Box, Button, Container, TextField } from "@mui/material";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Items, Order } from "../../lib/models/order";
 
 // TODO: pull from DB or hardcode into file
-const menuItems = ["Matcha (Cold)", "Matcha (Hot)", "Viet Coffee (Cold)", "Viet Coffee (Hot)"," Earl Gray Cupcake"]
+const menuItems = ["Matcha (Cold)", "Matcha (Hot)", "Coffee (Cold)", "Coffee (Hot)"," Earl Gray Cupcake", "Coconut Coffee (Cold)", "Cocunut Coffee (Hot)", "Blueberry Matcha (Cold)", "Blueberry Matcha (Hot)", "Banana Bread"];
 
 export default function Ordering() {
   const router = useRouter();
@@ -39,52 +39,86 @@ export default function Ordering() {
   }
 
   return (
-    <Container>
+    <Container sx={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
       <Box>
-        <Box>
-          Customer Name
-        </Box>
-        <Box>
-          <TextField onChange={(e) => {setName(e.target.value)}}/>
-        </Box>
+        <Typography variant="h3">
+          Create an order
+        </Typography>
       </Box>
-      <Box>
-        <Box>
-          Menu Items
-        </Box>
-        <Box>
-          {menuItems.map((item, index) => (
-            <Button key={index} onClick={() => addItemToOrder(item)}>
-              {item}
-            </Button>
-          ))}
-        </Box>
-      </Box>
-      <Box>
-        {Object.entries(items).map(([item, quantity]) => (
-          <Box key={item}>
-            <Box>
-              {item}
-            </Box>
-            <Box>
-              {quantity}
-            </Box>
-            <Box>
-              TODO: add increase/decrease quantities
-            </Box>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        gap: "2"
+      }}>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+          <Typography variant="h5">
+            Menu Items
+          </Typography>
+          <Box sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gridTemplateRows: "repeat(5, auto)",
+            gap: "10px",
+            flex: "2"
+          }}>
+            {menuItems.map((item, index) => (
+              <Button key={index} onClick={() => addItemToOrder(item)} sx={{
+                backgroundColor: "lightblue",
+                padding: "10px",
+                textAlign: "center"
+              }}>
+                {item}
+              </Button>
+            ))}
           </Box>
-        ))}
+        </Box>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+          {Object.entries(items).map(([item, quantity]) => (
+            <Box key={item}>
+              <Box>
+                {item}
+              </Box>
+              <Box>
+                {quantity}
+              </Box>
+              <Box>
+                TODO: add increase/decrease quantities
+              </Box>
+            </Box>
+          ))}
+          <Box>
+            <Typography>Total: ${total}</Typography>
+          </Box>
+        </Box>
       </Box>
-      <Box>
-        <Box>
-          Total
+      <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+          <Typography variant="h5">Customer Name</Typography>
+          <Box>
+            <TextField onChange={(e) => {setName(e.target.value)}}/>
+          </Box>
         </Box>
         <Box>
-          {total}
+          <Button onClick={() => submitOrder()}>Submit Order</Button>
         </Box>
-      </Box>
-      <Box>
-        <Button onClick={() => submitOrder()}>Submit Order</Button>
       </Box>
     </Container>
   );
